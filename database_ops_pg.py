b5d52d2613bb36eb6070307b5160eeb702c9fdc2
mehran-sarmadi <mehran.sarmadi16@gmail.com>
2025-11-17 16:45:11 +0330

    modigy ops pg

--- database_ops_pg.py
+++ database_ops_pg.py
@@ -8,24 +8,28 @@ from tqdm import tqdm
 import time
 import pandas as pd
 import json
+from openai import OpenAI
+
+
 load_dotenv()
+
+client = OpenAI(
+    api_key="not-needed",
+    base_url="http://172.23.0.3:8002/v1"
+)
 
 print("Defining Postgres table schemas...")
 POSTGRES_TABLES = {
     "categories": {
         "category": "TEXT PRIMARY KEY",
         "count": "INT",
-        "embeddings": "FLOAT8[]",
-        "created_at": "TIMESTAMP",
-        "updated_at": "TIMESTAMP"
+        "embedding": "FLOAT8[]",
     },
     "subcategories": {
         "category": "TEXT",
         "subcategory": "TEXT",
         "count": "INT",
-        "embeddings": "FLOAT8[]",
-        "created_at": "TIMESTAMP",
-        "updated_at": "TIMESTAMP",
+        "embedding": "FLOAT8[]",
     },
 }
 
@@ -66,20 +70,49 @@ with open("/home/mehran/Mehran/work/mcinext/cca/cca-db/test_subcategory_handcraf
 
 print("Inserting categories...")
 for category in tqdm(test_subcategory.keys(), total=len(test_subcategory.keys())):
+    resp = client.embeddings.create(
+                                model="hakim-unsup",
+                                input=[category]
+                            )
+    embedding = resp.data[0].embedding
     database_operator.add_or_increment("categories", {
         "category": category,
-        "embeddings": [random.random() for _ in range(768)],  # Assuming 768-dimensional embeddings
+        "embedding": embedding
     })
 
 print("Inserting subcategories...")
 for key, value in tqdm(test_subcategory.items(), total=len(test_subcategory)):
     for v in value:
+        resp = client.embeddings.create(
+                                model="hakim-unsup",
+                                input=[v]
+                            )
+        embedding = resp.data[0].embedding
         database_operator.add_or_increment("subcategories", {
             "subcategory": v,
             "category": key,
-            "embeddings": [random.random() for _ in range(768)],  # Assuming 768-dimensional embeddings
+            "embedding": embedding
         })
 
 
 print(category_table.columns)
 print(subcategory_table.columns)
+
+
+category_table = database_operator.get_table_as_dataframe("categories")
+print(f"Existing categories in DB: {len(category_table)}")
+
+subcategory_table = database_operator.get_table_as_dataframe("subcategories")
+print(f"Existing subcategories in DB: {len(subcategory_table)}")
+
+
+print(category_table.head())
+print(subcategory_table.head())
+
+
+
+
+
+
+
+
